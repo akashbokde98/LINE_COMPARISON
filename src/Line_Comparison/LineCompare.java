d0ee2fbaafb78ef0956a0739e037bb65786e2907
akashbokde98 <akashbokde98@gmail.com>
2023-04-12 18:28:33 +0530

    Added:Displayed Welcome Message

---
+++ src/Line_Comparison/LineCompare.java
@@ -0,0 +1,9 @@
+package Line_Comparison;
+
+public class LineCompare
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Line Comparison Computation Problem!");
+    }
+}
